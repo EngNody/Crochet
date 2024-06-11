caa42363cd6ea89eff68b0b1e59226bc30e1fda9
EngNody <totatota1990253@gmail.com>
2024-06-11 06:17:59 +0300

    .

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,10 +1,63 @@
-import React from 'react'
-import './ListProduct.css'
+import React, {useEffect , useState } from 'react'
+import './ListProduct.css';
+// import cross_icon from '../../assets/Waving-Hand.png'
+import cross_icon from '../../assets/remove-icon.jpg'
 
 const ListProduct = () => {
+
+const [allproducts , setAllProducts]=useState([]);
+
+const fetchInfo = async ()=>{
+  await fetch('http://localhost:4000/allproducts')
+  .then((res)=>res.json())
+  .then((data)=>{setAllProducts(data)})
+}
+
+    useEffect(()=>{
+      fetchInfo();
+    }, []);
+
+const remove_product= async (id)=>{
+  await fetch('http://localhost:4000/removeproduct',{
+    method:'POST',
+    headers:{
+      Accept:'application/json',
+      'Content-Type':'application/json',
+    },
+    body:JSON.stringify({id:id})
+})
+await fetchInfo();
+}
+    
   return (
-    <div className='list-product'>ListProduct</div>
+    <div className='list-product'>
+    <h1>All Products List</h1>
+    <div className="listproduct-format-main">
+      <p>Products</p>
+      <p className='justify-self'>Title</p>
+      <p>Old Pricde</p>
+      <p>New Price</p>
+      <p>Category</p>
+      <p>Remove</p>
+    </div>
+    <div className="listproduct-allproducts">
+      <hr/>
+    {allproducts.map((product,index)=>{
+      return <>
+      <div key={index} className='listproduct-format-main listproduct-format'>
+          <img src={product.image} alt="" className="listproduct-product-icon" />
+          <p className='justify-self' id='theTitle'>{product.name}</p>
+          <p>${product.old_price}</p>
+          <p>${product.new_price}</p>
+          <p>{product.category}</p>
+          <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' style={{alignSelf:"center"}}  src={cross_icon} alt="" />
+      </div>
+      <hr/>
+      </>
+    })}
+    </div>
+    </div>
   )
 }
 
-export default ListProduct
+export default ListProduct;
--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -3,7 +3,7 @@ import './Admin.css';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import { Routes,Route } from 'react-router-dom'
 import AddProduct from '../../Components/AddProuduct/AddProduct';
-import ListProduct from '../../Components/AddProuduct/AddProduct';
+import ListProduct from '../../Components/ListProduct/ListProduct';
 
 const Admin = () => {
   return (
